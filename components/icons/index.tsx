e9321062c6965ce43afa0a9e28e912a84b0684ea
oladepotesleem5 <oladepotesleem5@gmail.com>
2024-10-01 07:45:07 +0000

    first commit with navbar and icons made

---
+++ components/icons/index.tsx
@@ -0,0 +1,8 @@
+import Burger from "./Burger"
+import Closer from "./Closer"
+
+
+export {
+    Burger,
+    Closer
+}
---
+++ components/ui/navbar/index.tsx
@@ -0,0 +1,57 @@
+"use client"
+import {Closer, Burger } from "@/components/icons";
+import { useState } from "react"
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <header className="flex justify-between w-full items-center">
+      <div>
+        SleepStiq
+      </div>
+    <nav >
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              {isOpen ? (
+                <Burger />
+              ) : (
+                <Closer />
+              )}
+            </button>
+          </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
+                <a href="#" className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+                <a href="#" className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+                <a href="#" className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Shop</a>
+                <a href="#" className="hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">FAQs</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={`${isOpen ? 'block' : 'hidden'} sm:hidden`} id="mobile-menu">
+        <div className="px-2 pt-2 pb-3 space-y-1">
+          <a href="#" className="hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Home</a>
+          <a href="#" className="hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">About</a>
+          <a href="#" className="hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Shop</a>
+          <a href="#" className="hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">FAQs</a>
+        </div>
+      </div>
+    </nav>
+    </header>
+  );
+};
+
+export default Navbar;
+
