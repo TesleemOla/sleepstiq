be2f634a25d572e3b936e7caa8392193d1893171
Tasleemofx <oladepotesleem5@gmail.com>
2024-10-07 12:40:05 +0100

    footer small screen style fix

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 import { Logo, Facebook, Twitter, Gplus, LinkedIn } from "../icons";
 
 export default function Footer(){
-    return <footer className="bg-primary text-white w-screen sm:w-full px-2">
+    return <footer className="bg-primary text-white w-screen sm:w-full px-4">
         <div className="flex justify-center py-5 scale-50 md:scale-100">
             <Logo loc="footer"/>
         </div>
-        <div className="flex flex-row gap-5 flex-wrap flex-auto justify-center">
+        <div className="flex flex-col text-center items-center sm:flex-row gap-5 sm:flex-wrap justify-center">
         <div className="flex flex-col gap-7 basis-1/6">
             <p className="font-bold text-sm">COMPANY</p>
             <Link href="/about" className="sm:font-thin text-lg sm:text-xs">About</Link>
@@ -22,7 +22,7 @@ export default function Footer(){
             <p className="sm:font-thin text-lg sm:text-xs">Address</p>
             <p className="text-xs font-extrabold">16, Ogundipe Close, Upston Close</p>
         </div>
-        <div className="flex flex-col gap-10 basis-2/6 flex-shrink">
+        <div className="flex flex-col gap-10 basis-2/6 flex-shrink w-1/2 sm:w-2/3">
             <p className="font-bold text-sm">CONSUMER ADVISORY</p>
             <p className="font-thin leading-loose text-xs sm:text-sm">
                     These statements have not been evaluated by the Food and Drug Administration.
